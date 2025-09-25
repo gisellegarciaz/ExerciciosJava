4013c7f83a324be9eb45e2327b2915bf737b14bb
Giselle Garcia <gisellepagarcia@gmail.com>
2025-09-24 21:11:07 -0300

    Commit Projeto Cartas

---
+++ ExercGi_JOp_CartasMotivacionais/src/classeCartasMotivacionais/ClasseCartasMotivacionais.java
@@ -0,0 +1,16 @@
+package classeCartasMotivacionais;
+
+import java.util.Random;
+
+public class ClasseCartasMotivacionais {
+	
+	private String[] frases = {
+	        "Acredite em si! O sucesso é resultado de sua persistência.",
+	        "Você é capaz! Transforme seus desafios em oportunidades de crescimento.",
+	        "Tenha coragem! O primeiro passo é sempre o mais importante.",
+	        "Mantenha-se forte! A superação é a prova de sua resiliência.",
+	        "Inspire-se! Sua criatividade é a chave para a inovação."
+	    };
+	
+	
+}
